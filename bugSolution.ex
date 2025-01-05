35574bffda5817aa76cc1e60cb45bae3bee9b378
BugHunter-X <askzenai@gmail.com>
2025-01-05 16:52:05 +0100

    Add bugSolution.ex file

---
+++ bugSolution.ex
@@ -0,0 +1,14 @@
+```elixir
+list = [1, 2, 3, 4, 5]
+
+try do
+  Enum.each(list, fn x ->
+    if x == 3 do
+      throw(:three_found)
+    end
+    IO.puts(x)
+  end)
+rescue
+  :three_found -> IO.puts("Number 3 found, gracefully exiting.")
+end
+```
